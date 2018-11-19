105db0997a7007158d18c506e71c5d63a23217ee
Sydney Steel <sydneysteel@Sydneys-MacBook-Air-2.local>
2018-11-19 18:32:33 -0500

    reformating labels and drop-down

--- PS_7/app.R
+++ PS_7/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
+library(ggrepel)
 
 App_data <- read_csv("PS_7.csv")
 
@@ -34,6 +35,7 @@ ui <- fluidPage(
 
       #create checkbox for user input of states
       checkboxGroupInput(inputId = "selected_type",
+                         width = 50,
                          label = "Select State(s):",
                          choices = c("AZ","CA","CO","FL","GA","IL","IA","KS","KY","ME","MI","MN","NJ","NM","NY","NC","OH","PA","TX","UT","VA","WA","WV"),
                          selected = c("AZ","CA","CO","FL","GA","IL","IA","KS","KY","ME","MI","MN","NJ","NM","NY","NC","OH","PA","TX","UT","VA","WA","WV"))
@@ -44,7 +46,7 @@ ui <- fluidPage(
     # Outputs
     
     mainPanel(
-      plotOutput(outputId = "scatterplot"),
+      plotOutput(outputId = "scatterplot", height = 600, width = 800),
       p("Choose from the different parties to see how polling margin of errors vary with voter turnout."),
       p("You can also select which state elections you would like to view.")
       
@@ -72,6 +74,7 @@ server <- function(input, output) {
       geom_abline() +
       scale_color_manual(labels = c("D" = "Democratic Party", "R" = "Republican Party", "UNDECIDED" = "Undecided"),
                          values = c("blue", "red", "grey")) +
+      geom_label_repel(aes(label = state_dist), size = 4, force = 3) +
       labs(color = "Winning Party", x = "Voter Turnout") +
       ggtitle("Margin of Error vs Voter Turnout by District")
   })
