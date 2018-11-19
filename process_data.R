5f7458b457bf6f3a2875b5987dbe7c018883fbfd
Charles Flood <charlesflood@Charless-MacBook-Pro.local>
2018-11-19 16:37:52 -0500

    First Commit - organizing app data w/ Sydney

---
+++ process_data.R
@@ -0,0 +1,70 @@
+
+# Install libraries
+
+library(tidyverse)
+library(dplyr)
+library(stringr)
+library(fs)
+library(reshape2)
+library(knitr)
+library(lubridate)
+library(scales)
+library(foreign)
+library(janitor)
+library(data.table)
+
+#  load in Results data
+
+results <- read_csv("mt_2_results.csv") %>% mutate(state_dist = paste(str_to_lower(state), district, sep = "")) %>% 
+  mutate(total = dem_votes + rep_votes + other_votes, dem = dem_votes/total, rep = rep_votes/total, other = other_votes/total)
+
+# load in upshot data
+
+download.file(url = "https://goo.gl/ZRCBda",
+              destfile = "master.zip",
+              quiet = TRUE,
+              mode = "wb")
+
+unzip("master.zip")
+
+my_list <- dir_ls("2018-live-poll-results-master/data/")
+
+# Combine all upshot data into one dataframe
+
+predictions <- map_dfr(my_list, read_csv, .id = "source") 
+
+# Create variables for state, district and state_district combo and filter for wave 3
+
+pred_wave_3 <- predictions %>%  mutate(wave = str_sub(source, -5,-5), state = str_sub(source, 51,52), district = str_sub(source,53,54), district = as.numeric(district), state_dist = str_extract(source, "[a-zA-Z]{2}[0-9]{2}")) %>% 
+  filter(wave == 3)
+
+# Clean errors in response variable
+
+pred_wave_3$response[pred_wave_3$response==3] <- "Und"
+pred_wave_3$response[pred_wave_3$response==4] <- "Und"
+pred_wave_3$response[pred_wave_3$response==5] <- "Und"
+pred_wave_3$response[pred_wave_3$response==6] <- "Und"
+
+# collapse upshot data into district level
+
+ pred_district <- pred_wave_3 %>% 
+   select(response, final_weight, state_dist, wave) %>% 
+   filter(!is.na(final_weight)) %>% 
+   group_by(response, state_dist) %>% 
+   tally(wt = final_weight) %>% 
+   group_by(state_dist) %>% 
+   spread(key = response, value = n) %>% 
+   filter(!is.na(state_dist)) %>% 
+   group_by(state_dist) %>% 
+   mutate(total = Dem + Rep + Und, Dem = Dem/total, Rep = Rep/total, Und = Und/total)
+ 
+ 
+ 
+ #Final dataframe combining both results and predictions for wave 3 polling
+ 
+ final <- left_join(results, pred_district, by = "state_dist") %>% 
+   filter(!is.na(Dem))
+  
+
+
+
